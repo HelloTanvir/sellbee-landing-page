b79df6f0bb3e2760aedbc4afca1e73391fc4a791
HelloTanvir <69837408+HelloTanvir@users.noreply.github.com>
2021-10-30 23:47:51 +0600

    logo centered

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -5,9 +5,13 @@ import SmHeaderRight from './Sm-header-right';
 
 const Header = () => (
     <div className="sticky top-0 z-10 flex flex-row-reverse h-16 text-right text-white border-b border-gray-300 lg:flex-row bg-gradient-to-r from-bg-banner-left to-bg-banner-right">
-        <div className="flex items-center pr-8 lg:pl-8 lg:pr-0">
+        <div className="flex items-center flex-1 pr-8 lg:pl-8 lg:pr-0">
             <Link href="/">
-                <img src="/logo-dark.svg" alt="logo" className="h-12 cursor-pointer" />
+                <img
+                    src="/logo-dark.svg"
+                    alt="logo"
+                    className="h-12 transform -translate-x-1/2 cursor-pointer"
+                />
             </Link>
         </div>
 
