7f17eb34d0fa7d23d95a6639c8550351d61fc6f8
HelloTanvir <69837408+HelloTanvir@users.noreply.github.com>
2021-11-29 21:11:55 +0600

    visitor counter added using countAPI-js

--- src/components/clientCounter/ClientCounter.tsx
+++ src/components/clientCounter/ClientCounter.tsx
@@ -1,49 +1,36 @@
-import React from 'react';
+import countApi from 'countapi-js';
+import React, { useEffect, useState } from 'react';
+import CountUp from 'react-countup';
 
-const ClientCounter = () => (
-    // const nodeRef = useRef<HTMLDivElement>();
+const ClientCounter = () => {
+    const [visitorCount, setVisitorCount] = useState(0);
+    useEffect(() => {
+        const countVisitors = async () => {
+            const count = await countApi.get('sellbeez.com', 'visits');
+            setVisitorCount(count.value);
+        };
 
-    // const startNumber = 0;
-    // const endNumber = 28;
+        countVisitors();
+    }, []);
 
-    // useEffect(() => {
-    //     const node = nodeRef.current;
+    return (
+        <div className="flex flex-col items-center pt-32 space-y-4">
+            <span className="text-xs tracking-wide text-gray-500 uppercase">Early Bees</span>
 
-    //     const controls = animate(startNumber, endNumber, {
-    //         duration: 2,
-    //         onUpdate(value) {
-    //             if (node) {
-    //                 // eslint-disable-next-line radix
-    //                 node.textContent = `${parseInt(`${value}`)}`;
-    //             }
-    //         },
-    //     });
+            <div className="flex flex-col gap-4">
+                <span className="text-2xl tracking-wide capitalize lg:text-4xl">
+                    people who registered early
+                </span>
+                <span className="bg-gradient-to-r from-white to-title-border h-0.5 w-48 mx-auto" />
+            </div>
 
-    //     return () => controls.stop();
-    // }, []);
-
-    <div className="flex flex-col items-center pt-32 space-y-4">
-        <span className="text-xs tracking-wide text-gray-500 uppercase">Early Bees</span>
-
-        <div className="flex flex-col gap-4">
-            <span className="text-2xl tracking-wide capitalize lg:text-4xl">
-                people who registered early
-            </span>
-            <span className="bg-gradient-to-r from-white to-title-border h-0.5 w-48 mx-auto" />
-        </div>
-
-        {/* <div
-                className="px-20 text-6xl text-white bg-black rounded-md py-7"
-                ref={nodeRef}
+            <div
+                className="px-20 text-5xl text-white bg-black rounded-md lg:text-6xl py-7"
                 style={{ marginTop: 44 }}
-            /> */}
-
-        <div
-            className="px-20 text-5xl text-white bg-black rounded-md lg:text-6xl py-7"
-            style={{ marginTop: 44 }}
-        >
-            79
+            >
+                <CountUp end={visitorCount} duration={0.2} />
+            </div>
         </div>
-    </div>
-);
+    );
+};
 export default ClientCounter;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,17 @@
+import countApi from 'countapi-js';
 import type { AppProps } from 'next/app';
+import { useEffect } from 'react';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
+    useEffect(() => {
+        const countVisitors = async () => {
+            await countApi.hit('sellbeez.com', 'visits');
+        };
+
+        countVisitors();
+    }, []);
+
     return <Component {...pageProps} />;
 }
 export default MyApp;
