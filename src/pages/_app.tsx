9c45063e332c4f638d2af68ef8a1ec77023c383b
HelloTanvir <69837408+HelloTanvir@users.noreply.github.com>
2021-11-30 12:01:15 +0600

    google analytics added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,15 @@
 import countApi from 'countapi-js';
 import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 import ReactGa from 'react-ga';
 import '../styles/globals.css';
 
+ReactGa.initialize(process.env.NEXT_PUBLIC_ANALYTICS_TRACKING_ID || '');
+
 function MyApp({ Component, pageProps }: AppProps) {
+    const router = useRouter();
+
     useEffect(() => {
         const countVisitors = async () => {
             await countApi.hit('sellbeez.com', 'visits');
@@ -14,10 +19,10 @@ function MyApp({ Component, pageProps }: AppProps) {
     }, []);
 
     useEffect(() => {
-        ReactGa.initialize('G-MT27R677RQ');
+        console.log('in ga');
 
         ReactGa.pageview(window.location.pathname + window.location.search);
-    }, []);
+    }, [router.pathname]);
 
     return <Component {...pageProps} />;
 }
